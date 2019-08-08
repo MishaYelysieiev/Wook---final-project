4c6a85918042ddca42ab8295e9f0871a16316e44
hliadchyshyn <39763795+hliadchyshyn@users.noreply.github.com>
2019-08-08 18:20:57 +0300

    basic folder structure + packages (#2) (#3)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {connect} from 'react-redux';
+import {reducer as formReducer} from 'redux-form';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>WOOK</h1>
       </header>
     </div>
   );
