e7c72acd0d2e468969f4bfa8cc32c8cce42b0b69
hliadchyshyn <i.s.hliadchyshyn@gmail.com>
2019-08-12 15:56:41 +0300

    Added mongoDB connection function, installed mongoose, nodemon

---
+++ src/backend/server.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// const Data = require('./data');
+
+const API_PORT = 3000;
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+// this is our MongoDB database
+const dbRoute= require('./config');
+
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// connects our back end code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+
+// append /api for our http requests
+app.use('/api', router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
