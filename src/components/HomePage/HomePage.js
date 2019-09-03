66933040a6b203ad2aa9cd53c284401d4663e7c9
MaksymHilhurt <48875262+MaksymHilhurt@users.noreply.github.com>
2019-09-03 20:37:33 +0300

    Main category section (#14)
    * basic folder structure + packages
    
    * basic folder structure + packages (#2) (#3)
    
    * header-comp+banner-comp__desktop-version
    
    * COMMENTS FOR CATEGORY SECTION
    
    * src/index.scss manually added
    
    * MainCategorySection initial commit
    
    * Update index.scss

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 import Banner from '../Banner/Banner';
+import MainCategorySection from "../MainCategorySection/MainCategorySection";
+
 import SubscribeSection from '../SubscribeSection/SubscribeSection';
 
 
 import './HomePage.scss';
+
 
 
 
@@ -16,6 +19,8 @@ class HomePage extends React.Component {
         return (
             <main className='HomePage'>
                 <Banner/>
+                <MainCategorySection title='New Books'/>
+                <MainCategorySection title='Most Popular Books'/>
             <SubscribeSection/>
 
             </main>
