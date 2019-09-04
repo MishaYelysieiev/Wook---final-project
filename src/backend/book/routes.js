6e74a9324383600f97c2ec9a5a68533ecdefae58
hliadchyshyn <i.s.hliadchyshyn@gmail.com>
2019-09-04 13:16:09 +0300

    added fetch book by id

--- src/backend/book/routes.js
+++ src/backend/book/routes.js
@@ -14,7 +14,10 @@ module.exports = (app) => {
     app.post('/book_search', books.findBooksBySearch);
 
     // Retrieve a single Book with categoryName
-    app.get('/book/:category', books.findByCategory);
+    app.get('/book/category/:category', books.findByCategory);
+
+     // Retrieve a single Book with categoryName
+     app.get('/book/:id', books.findById);
 
     // Update a Book with bookId
     app.put('/book/:id', books.update);
