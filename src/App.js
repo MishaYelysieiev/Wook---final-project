93a3c9a188aeef9d37cd69276da19f29a80f00f7
Marina Zhuk <marinazhuk603@gmail.com>
2019-09-17 13:24:24 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import CategorySection from './components/CategorySection/CategorySection';
 import Footer from './components/Footer/Footer';
+import Cart from './components/Cart/Cart';
 
 import ProductCardInfo from './components/ProductCardInfo/ProductCardInfo';
-
 
 
 
@@ -17,15 +17,21 @@ import ProductCardInfo from './components/ProductCardInfo/ProductCardInfo';
 import './App.scss';
 
 function App() {
+
     return (
         <div className="App">
             <Header/>
             <Route exact path='/' component={HomePage}/>
             <Route path='/category/:category' component={CategorySection}/>
+            <Route path='/book_search' component={CategorySection}/>
+            <Route path='/sale' component={CategorySection}/>
             <Route path='/book/' component={ProductCardInfo}/>
+            <Route path='/cart' component={Cart}/>
             <Footer/>
         </div>
-    );
+
+);
+
 }
 
 export default App;
