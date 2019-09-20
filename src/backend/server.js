67a8ab5296c3ade56ec89a9b6d8cb81f75e18b0a
hliadchyshyn <i.s.hliadchyshyn@gmail.com>
2019-09-20 16:40:09 +0300

    changes for deployment

--- src/backend/server.js
+++ src/backend/server.js
@@ -13,7 +13,11 @@ const config = process.env // for localhost have to be comment
 const mongoose = require('mongoose');
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build/')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 // parse requests
 app.use(bodyParser.urlencoded({extended: true}));
