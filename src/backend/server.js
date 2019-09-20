fe4466e65137a783f3b3a7c2f23ca5e2c4785219
hliadchyshyn <39763795+hliadchyshyn@users.noreply.github.com>
2019-09-20 13:13:09 +0300

    added methods for orders (#43)

--- src/backend/server.js
+++ src/backend/server.js
@@ -28,8 +28,9 @@ mongoose.Promise = global.Promise;
 //routes file here
 require('./category/routes')(app);
 require('./book/routes')(app);
-require('./cart/routes')(app);
 require('./user/routes')(app);
+require('./order/routes')(app);
+
 
 // connects our back end code with the database
 // Connecting to the database
