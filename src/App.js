2918c0a97d37371b8e89ea5afd70c456d02c8964
hliadchyshyn <i.s.hliadchyshyn@gmail.com>
2019-09-25 13:13:31 +0300

    Added search result page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {reducer as formReducer} from 'redux-form';
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import CategorySection from './components/CategorySection/CategorySection';
+import SearchResultSection from './components/SearchResultSection/SearchResultSection';
 import Cabinet from './components/Cabinet/Cabinet'
 import Footer from './components/Footer/Footer';
 import Cart from './components/Cart/Cart';
@@ -24,7 +25,7 @@ function App() {
             <Header/>
             <Route exact path='/' component={HomePage}/>
             <Route path='/category/:category' component={CategorySection}/>
-            <Route path='/book_search' component={CategorySection}/>
+            <Route path='/book_search/:search' component={SearchResultSection}/>
             <Route path='/sale' component={CategorySection}/>
             <Route path='/book/' component={ProductCardInfo}/>
             <Route path='/cart' component={Cart}/>
