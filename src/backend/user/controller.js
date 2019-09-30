218c9ad834ae958ab151b171696210cb43425d14
hliadchyshyn <9763795+hliadchyshyn@users.noreply.github.com>
2019-09-30 23:26:00 +0300

    added check for updating password

--- src/backend/user/controller.js
+++ src/backend/user/controller.js
@@ -186,10 +186,24 @@ exports.update = async (req, res) => {
       address
     } = req.body;
 
-    // Crypt the password
+
+    // Find the user
+    const user = await User.findOne({
+      email
+    });
+    if (!user) {
+      errors.email = 'User not found!'
+      return res.status(404).json(errors);
+    }
+    // check if password changed
+    const samePassword = await password === user.password;
+    // userInfo
     const salt = await bcrypt.genSalt(10);
-    // userInfo.
-    password = await bcrypt.hash(password, salt);
+    if (!samePassword){
+      password = await bcrypt.hash(password, salt);
+    } else {
+      password = await user.password
+    }
     // Update the user
     const newUser = await User.findByIdAndUpdate(req.user._id, {
       firstName,
